215314efb8686467e86a848081004c68ef0776e4
Yunussov <ext.baisangur.yunussov@sgs.com>
2024-07-22 17:06:02 +0500

    Longest substring & longest prefix

---
+++ LongestCommonPrefix/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string longest = "";
+        int smallest = Int32.MaxValue;
+        bool isSatisfied = true;
+        foreach (var str in strs)
+        {
+            if (str.Length < smallest)
+                smallest = str.Length;
+        }
+        for (int i = 1; i < smallest + 1 && isSatisfied; i++)
+        {
+            string candidate = strs[0].Substring(0, i);
+            foreach (var str in strs)
+            {
+                if (!(str.Substring(0, i) == candidate))
+                    isSatisfied = false;
+            }
+            if (isSatisfied)
+                longest = candidate;
+        }
+        return longest;
+    }
+}
---
+++ LongestSubstringWithoutRepeating/Solution.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int maxLen = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            string sub = "" + s[i];
+            int len = 1;
+            bool isValid = true;
+            for (int j = i + 1; j < s.Length && isValid; j++)
+            {
+                if (Contains(sub, s[j]))
+                {
+                    isValid = false;
+                    break;
+                }
+                else
+                    len++;
+                sub += s[j];
+            }
+            if (len > maxLen)
+                maxLen = len;
+        }
+
+        return maxLen;
+    }
+
+    public bool Contains(string s, char c)
+    {
+        bool isContains = false;
+
+        foreach (var i in s)
+        {
+            if (i == c)
+                isContains = true;
+        }
+
+        return isContains;
+    }
+}
